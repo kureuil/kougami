6eaf342e8486017383bcf269a2e00c5aa9d422ce
Louis Person <lait.kureuil@gmail.com>
2017-02-12 16:01:16 +0100

    Print debug messages on stderr

--- src/kougami/linters/NoSpaceAfterUnary.cpp
+++ src/kougami/linters/NoSpaceAfterUnary.cpp
@@ -7,7 +7,7 @@ namespace linters {
 Result NoSpaceAfterUnary::visit_unary_operator(Cursor const &cursor,
                                                Cursor const &) {
   // if (cursor.location().line())
-  std::cerr << cursor.location() << ": " << cursor.extent().start().column()
+  std::cerr << cursor.location() << ": {" << cursor.spelling() << "} " << cursor.extent().start().column()
             << "--" << cursor.extent().end().column() << std::endl;
   return Result::Recurse;
 }
