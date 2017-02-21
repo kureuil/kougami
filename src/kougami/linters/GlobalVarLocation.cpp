633c34ac61d5da7f84dc64cc240e5efe9aeb8d60
Louis Person <lait.kureuil@gmail.com>
2017-02-21 17:53:52 +0100

    Fox invalid warning when defining global variable in header file

--- src/kougami/linters/GlobalVarLocation.cpp
+++ src/kougami/linters/GlobalVarLocation.cpp
@@ -3,7 +3,11 @@
 namespace kougami {
 namespace linters {
 Result GlobalVarLocation::visit_var_decl(Cursor const &cursor, Cursor const &) {
-  this->check_cursor_is_in_source(cursor);
+  if (cursor.storage_class() == CX_SC_Extern || cursor.storage_class() == CX_SC_PrivateExtern) {
+    this->check_cursor_is_in_header(cursor);
+  } else {
+    this->check_cursor_is_in_source(cursor);
+  }
   return Result::Recurse;
 }
 }
