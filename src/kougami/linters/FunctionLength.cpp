7e51636b738f87a31b4c908cbe4f881f37dcb723
Louis Person <lait.kureuil@gmail.com>
2017-02-23 11:39:00 +0100

    Fix function length computation

--- src/kougami/linters/FunctionLength.cpp
+++ src/kougami/linters/FunctionLength.cpp
@@ -9,7 +9,7 @@ Result FunctionLength::visit_compound_stmt(Cursor const &cursor,
   if (parent.kind() != CXCursor_FunctionDecl) {
     return Result::Recurse;
   }
-  if (cursor.extent().end().line() - cursor.extent().start().line() > 25) {
+  if (cursor.extent().end().line() - cursor.extent().start().line() - 1 > 25) {
     this->_errors.emplace_back(
         Error{cursor.location(), "function must not be longer than 25 lines"});
   }
